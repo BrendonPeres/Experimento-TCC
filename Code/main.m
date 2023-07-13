4c2a09da10b552d803221f1752bc64b21a363e5b
Brendon <brendon.peres@gmail.com>
2023-07-13 17:17:55 -0300

    Add files via upload

---
+++ Code/main.m
@@ -0,0 +1,56 @@
+clear
+clc
+
+dado = struct("metodo", "qam", ...
+                    "grau", 4);
+
+piloto = struct("metodo",[], ...
+            "grau",[], ...
+            "razaoPilotoDado",[], ...
+            "localizacao",[]);
+
+piloto(1).metodo = ["qam" "qam" "qam" "qam"];
+piloto(1).grau = [4 4 4 4];
+piloto(1).razaoPilotoDado = [1 1 1 1];
+descricaoTeste(1) = "4*qam(4)";
+
+%% Usuário, alterações a partir desta linha. As modulações aceitas são: dpsk, pam, psk, qam.
+
+piloto(2).metodo = ["qam" "psk" "pam" "qam"];
+piloto(2).grau = [4 4 4 4];
+piloto(2).razaoPilotoDado = [1 1 1 1];
+descricaoTeste(2) = "qam(4) | psk(4) | pam(4) | qam(4)";
+
+piloto(3).metodo = ["qam" "qam" "qam" "qam"... 
+                    "qam" "qam" "qam" "qam"...
+                    "qam" "qam" "qam" "qam"...
+                    "qam" "qam" "qam" "qam"];
+piloto(3).grau = [4,4,4,4,... 
+                  4,4,4,4,...
+                  4,4,4,4,...
+                  4,4,4,4];
+                   
+piloto(3).razaoPilotoDado = [1,1,1,1,...
+                            1,1,1,1,...
+                            1,1,1,1,...
+                            1,1,1,1];
+
+descricaoTeste(3) = "16*qam(4)";
+
+
+% piloto(n)... Quantas pilotos quiser avaliar 
+
+quantidadePortadora = 256;
+tamanhoPrefixoCiclico = ceil(quantidadePortadora/4); % prefixo ciclico ou intervalo de guarda
+tamanhoCanal = 16;
+iteracoes = 1000;
+signalnoiseRatio_v = [-20:2:28];
+
+%% Simulações: NÃO ALTERAR
+% simulação das diferentes disposições de portadoras pelo code adaptado
+simular(dado,piloto,descricaoTeste,quantidadePortadora,tamanhoPrefixoCiclico,tamanhoCanal,iteracoes,signalnoiseRatio_v);
+simularOriginal(quantidadePortadora,length(piloto(1).grau),tamanhoPrefixoCiclico,tamanhoCanal,iteracoes,signalnoiseRatio_v);
+
+%% Desenho dos gráficos
+desenharGrafico(descricaoTeste,"Comparativo de diferentes designs de portadoras","northeast","fig1");
+desenharGrafico([descricaoTeste(1) "originalOutput"],"Código Adaptado x Código Original","southeast","fig2");
---
+++ Code/simularOriginal.m
@@ -0,0 +1,86 @@
+function simularOriginal(quantidadePortadora,quantidadePiloto,tamanhoPrefixoCiclico,tamanhoCanal,iteracoes,signalnoiseRatio_v)
+% Simula pelo arquivo base, o qual foi usado para ser adaptado. Foi
+% necessário adaptações mínimas sobre ele para que fizesse a configuração
+% de alguns parâmetros simultaneamente nos dois algoritmos de simulação.
+
+% in this Mfile, I want to investigate the performance of LSE algorithm in
+% OFDM channel estimation
+%
+% for further information see the 
+%       [Xiaodong Cai and Georgios B. Giannakis,
+%        Error Probability Minimizing Pilots for
+%        OFDM with M-PSK Modulation over Rayliegh-Fading
+%        Channels, IEEE Transactions on Vehicular 
+%        technology, vol. 53, No. 1, pp 146-155, Jan. 2004.]
+    
+    % parameter definition
+    N  = quantidadePortadora;           % total number of subchannels
+    P  = quantidadePiloto; %N/8;         % total number of Pilots
+    S  = N-P;           % totla number of data subchannels
+    GI = tamanhoPrefixoCiclico;           % guard interval length
+    M  = 4;             % modulation
+    pilotInterval = ceil(N/P);  % pilot position interval
+    L  = tamanhoCanal;            % channel length
+    nIteration = iteracoes;    % number of iteration in each evaluation
+    
+    SNR_V = signalnoiseRatio_v;   % signal to noise ratio vector in dB
+    ber = zeros(1,length(SNR_V));   % initializing bit error rate
+    
+    % Pilot Location and strength
+    Ip = [1:pilotInterval:N];       % location of pilots
+    Is = setxor(1:N,Ip);            % location of data
+        
+    Ep = 1;                        % energy in pilot symbols in comparison 
+                                        % to energy in data symbols
+    
+    % fft matrix
+    F = exp(2*pi*sqrt(-1)/N .* meshgrid([0:N-1],[0:N-1])...
+        .* repmat([0:N-1]',[1,N]));
+    
+    
+    for( i = 1 : length(SNR_V))
+        SNR = SNR_V(i);
+        for(k = 1 : nIteration)
+            % freq: Y=X.H + w=0
+            % generating random channel coefficients
+            h(1:L,1)  =     random('Normal',0,1,L,1) + j * random('Normal',0,1,L,1);  
+            h  = h./sum(abs(h));    % normalization    
+            
+            % Tr Data
+            TrDataBit = randi(M-1,N,1);%gera N valores de 0 ate M-1 em uma coluna (vetor)
+            TrDataMod = qammod(TrDataBit,M);
+            TrDataMod(Ip) = Ep * TrDataMod(Ip);
+            TrDataIfft = ifft(TrDataMod,N);
+            TrDataIfftGi = [TrDataIfft(N- GI + 1 : N);TrDataIfft];
+    
+    
+            % tx Data    
+            TxDataIfftGi = filter(h,1,TrDataIfftGi);    % channel effect CONVOLUCAO CIRCULAR!
+            % adding awgn noise
+            TxDataIfftGiNoise = awgn(TxDataIfftGi ...
+                , SNR - db(std(TxDataIfftGi))); % normalization to signal power
+    
+            TxDataIfft  = TxDataIfftGiNoise(GI+1:N+GI);
+            TxDataMod   = fft(TxDataIfft,N);
+    
+            % Channel estimation
+            Spilot = TrDataMod(Ip); % trnasmitted pilots
+            Ypilot = TxDataMod(Ip); % received pilots
+    
+            G = (Ep * length(Ip))^-1 ...
+                * ctranspose(sqrt(Ep)*diag(Spilot)*ctranspose(F(1:L,Ip)));
+    
+            hHat = G*Ypilot;    % estimated channel coefficient in time domain
+    
+            TxDataBit   = qamdemod(TxDataMod./(fft(hHat,N)),M);
+    
+            % bit error rate computation
+            [nErr bErr(i,k)] = symerr(TxDataBit(Is),TrDataBit(Is));
+        end
+    end
+    
+    snr = SNR_V';
+    bErr = mean(bErr')';
+    writetable(table(snr,bErr),'../Output/originalOutput.txt');
+
+end
